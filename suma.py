7eabd738d7d497e29cb760d3368020208e752e0b
Milanesa21 <diegojara88574@gmail.com>
2024-03-13 12:12:31 -0300

    first commit

---
+++ suma.py
@@ -0,0 +1,6 @@
+numero1 = int(input("Introduzca un numero: "))
+numero2 = int(input("Introduzca otro numero: "))
+
+resultado = (numero1) - (-numero2)
+
+print ("La suma de los valores es",resultado)
